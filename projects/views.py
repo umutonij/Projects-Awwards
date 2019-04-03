8e3e0b4130ef34f1f08e993c23ac75e4e2f6ed0e
umutoni jacqueline <jacquelineumutoni13@gmail.com>
2019-04-03 14:53:06 +0200

    add the route of the profile

--- projects/views.py
+++ projects/views.py
@@ -42,3 +42,19 @@ def new_project(request):
         else:
             form = NewProjectForm()
         return render(request, 'new_project.html', {"form": form,"current_user":current_user,"title":title})
+        
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+
+            return redirect('index')
+
+    else:
+        form = ProfileForm()
+    return render(request, 'pro_form.html', {"form": form})
