c7f0cae04a4c0d85e0cc2fcead007f0984e7093e
umutoni jacqueline <jacquelineumutoni13@gmail.com>
2019-04-03 15:07:08 +0200

    add the profile in urls of the app

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$', views.projects_today, name = 'projectsToday'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^new/project$', views.new_project, name='new-project')
+    url(r'^new/project$', views.new_project, name='new-project'),
+    url(r'^profile/', views.profile, name='profile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
